2f4df547a4a02b1f754859e1a7a8f3b5b4a95d83
Michael <m.paspaliaris@gmail.com>
2022-06-30 22:30:49 +1000

    completed the main arrow section

--- script.js
+++ script.js
@@ -4,11 +4,19 @@ let dulu = document.querySelector('.right-one');
  
 dulu.onclick = function() { 
   document.querySelector('.dulu-page').style.display = 'none'; 
+  document.querySelector('.constructorb-page').style.display = 'block'; 
   dulu.style.display = 'none';
   document.querySelector('.right-two').style.display = 'block'; 
   document.querySelector('.left-one').style.display = 'none'; 
   document.querySelector('.left-two').style.display = 'block'; 
-  document.querySelector('.left-two').style.opacity = "1"; 
+  document.querySelector('.left-two').style.opacity = "1";
+  document.getElementById("dulutitle").style.display = 'none';
+  document.getElementById("consttitle").style.display = 'inline';
+  document.getElementById("constructp").style.display = 'inline';
+  document.getElementById("dulup").style.display = 'none';
+
+  
+
 
 
 };
@@ -22,6 +30,13 @@ pageTwo.onclick = function() {
   pageTwo.style.display = 'none';
   document.querySelector('.right-three').style.display = 'block'; 
   document.querySelector('.right-three').style.opacity = "0.5"; 
+  document.querySelector('.left-three').style.display = "block"; 
+  document.querySelector('.left-two').style.display = "none"; 
+  document.querySelector('.left-three').style.opacity = "1"; 
+  document.getElementById("consttitle").style.display = 'none';
+  document.getElementById("fotomattitle").style.display = 'inline';
+  document.getElementById("constructp").style.display = 'none';
+  document.getElementById("fotomatp").style.display = 'inline';
 
 }
 
@@ -30,12 +45,21 @@ pageTwo.onclick = function() {
 let pageBackOne = document.querySelector('.left-two');
 
 pageBackOne.onclick = function() {
-  document.querySelector('.dulu-page').style.display = 'block'; 
-  dulu.style.display = 'block';
-  document.querySelector('.right-two').style.display = 'none'; 
   document.querySelector('.left-one').style.display = 'block'; 
+  document.querySelector('.right-one').style.display = 'block'; 
   document.querySelector('.left-two').style.display = 'none'; 
+  document.querySelector('.right-two').style.display = 'none'; 
+  document.querySelector('.left-three').style.display = 'none'; 
+  document.querySelector('.right-three').style.display = 'none'; 
   document.querySelector('.left-one').style.opacity = "0.5"; 
+  document.querySelector('.constructorb-page').style.display = 'none';
+  document.querySelector('.dulu-page').style.display = 'block'; 
+  document.getElementById("consttitle").style.display = 'none';
+  document.getElementById("dulutitle").style.display = 'inline';
+  document.getElementById("constructp").style.display = 'none';
+  document.getElementById("dulup").style.display = 'inline';
+ 
+
 
 }
 
@@ -43,10 +67,17 @@ let pageBackTwo = document.querySelector('.left-three');
 
 pageBackTwo.onclick = function() {
   document.querySelector('.constructorb-page').style.display = 'block'; 
-  document.querySelector('left').style.display = 'block';
+  document.querySelector('.right-two').style.display = 'block';
   document.querySelector('.right-three').style.display = 'none'; 
   document.querySelector('.left-two').style.display = 'block'; 
   document.querySelector('.left-three').style.display = 'none'; 
-  document.querySelector('.left-two').style.opacity = "0.5"; 
+  document.querySelector('.left-two').style.opacity = "1"; 
+  document.querySelector('.left-one').style.display = 'none'; 
+  document.querySelector('.right-one').style.display = 'none'; 
+  document.getElementById("consttitle").style.display = 'inline';
+  document.getElementById("fotomattitle").style.display = 'none';
+  document.getElementById("constructp").style.display = 'inline';
+  document.getElementById("fotomatp").style.display = 'none';
+
 
 }
